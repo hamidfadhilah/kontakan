d50516b00e18ffa5ebe74003d58ca8ea10ccacf8
root <root@unisba.ac.id>
2021-10-18 14:31:13 +0700

    Add all my files

---
+++ app/Models/Kegiatan.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class Kegiatan extends Model
+{
+    use HasFactory;
+
+    public function scopeCurrentUser($query)
+    {
+        return $query->where('id_user', Auth::user()->id);
+    }
+}
